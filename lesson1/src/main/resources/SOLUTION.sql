4e78c20e0b0bbdcfd60e6661b3525a1b60fb75b3
bakhridinova <bakhridinovaleyla@gmail.com>
2023-02-02 23:13:36 +0500

    updated SOLUTION.sql, TABLES_CHANGES.sql

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,2 +1,50 @@
+CREATE TABLE students
+(
+    id       BIGINT PRIMARY KEY auto_increment,
+    name     VARCHAR,
+    birthday DATE,
+    group    INT
+);
 
+CREATE TABLE student
+(
+    id          BIGINT NOT NULL auto_increment PRIMARY KEY,
+    name        VARCHAR(45),
+    birthday    DATE NOT NULL,
+    groupnumber INT NOT NULL
+);
 
+CREATE TABLE subject
+(
+    id          BIGINT NOT NULL auto_increment PRIMARY KEY,
+    name        VARCHAR(250),
+    description VARCHAR(255),
+    grade       INT NOT NULL
+);
+
+CREATE TABLE mark
+(
+    id         BIGINT NOT NULL auto_increment PRIMARY KEY,
+    student_id BIGINT,
+    subject_id BIGINT,
+    mark       INT NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES student(id),
+    FOREIGN KEY (subject_id) REFERENCES subject(id)
+);
+
+CREATE TABLE paymenttype
+(
+    id   BIGINT NOT NULL auto_increment PRIMARY KEY,
+    name VARCHAR(45)
+);
+
+CREATE TABLE payment
+(
+    id           BIGINT NOT NULL auto_increment PRIMARY KEY,
+    type_id      BIGINT NOT NULL,
+    amount       DECIMAL NOT NULL,
+    payment_date TIMESTAMP NOT NULL,
+    student_id   BIGINT NOT NULL,
+    FOREIGN KEY (type_id) REFERENCES paymenttype(id),
+    FOREIGN KEY (student_id) REFERENCES student(id)
+);
---
+++ lesson4/src/main/resources/SOLUTION.sql
@@ -0,0 +1,11 @@
+SELECT *
+FROM   student;
+
+SELECT TOP 50 *
+FROM   student;
+
+SELECT NAME
+FROM   student;
+
+SELECT DISTINCT amount
+FROM   payment;
